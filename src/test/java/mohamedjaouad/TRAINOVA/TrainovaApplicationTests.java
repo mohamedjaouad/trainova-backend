4e2b7828146bbcce003499ab7b93a735f7b48902
mohamed jaouad <jaouadmohamed080@gmail.com>
2026-08-29 11:48:18 +0200

    test: add initial context load test

---
+++ src/test/java/mohamedjaouad/TRAINOVA/TrainovaApplicationTests.java
@@ -0,0 +1,13 @@
+package mohamedjaouad.TRAINOVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
